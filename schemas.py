6b653b660dfee16fcdb85dce57302a3130f9c18e
anuragnitw6 <74757458+anuragnitw6@users.noreply.github.com>
2025-04-11 09:21:50 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -49,14 +49,10 @@ class SignupSchema(Schema):
             
 class UpdateProfileSchema(Schema):
     userid = fields.Str(required=True)
-    password = fields.Str(required=True)
     bms = fields.Bool(required=True)
     pushnotification = fields.Bool(required=True)
     aga = fields.Bool(required=True)
     qms = fields.Bool(required=True)
-    mobile = fields.Str(required=True)
-    username = fields.Str(required=True)
-    limit = fields.Str(required=True)
 
 class UserLogoutSchema(Schema):
     user_id = fields.Str(required=True)
